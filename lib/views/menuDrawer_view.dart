f29679410b5afe117e6c6b284bc084179f174dba
lucasmuniz95 <100134869+lucasmuniz95@users.noreply.github.com>
2022-10-10 18:11:26 -0300

    Update menuDrawer_view.dart

--- lib/views/menuDrawer_view.dart
+++ lib/views/menuDrawer_view.dart
@@ -5,30 +5,41 @@
 import 'package:flutter/material.dart';
 
 Widget menu(context) {
-  return Column(
-    children: [
-      UserAccountsDrawerHeader(
-          decoration: BoxDecoration(color: Colors.blue[900]),
-          accountName: const Text('EJC Santa Teresinha'),
-          accountEmail: const Text('Bandinha')),
-      ListTile(
-          leading: const Icon(Icons.home),
-          title: const Text('Inicio'),
-          onTap: () {
-            Navigator.of(context).pushReplacementNamed('/home');
-          }),
-      //ListTile(
-      //    leading: const Icon(Icons.music_note),
-      //    title: const Text('Adicionar musica'),
-      //    onTap: () {
-      //      Navigator.of(context).pushReplacementNamed('/add');
-      //    }),
-      ListTile(
-          leading: const Icon(Icons.exit_to_app),
-          title: const Text('Sair'),
-          onTap: () {
-            Navigator.of(context).pushReplacementNamed('/');
-          })
-    ],
+  return Drawer(
+    width: 200,
+    child: Column(
+      children: [
+        UserAccountsDrawerHeader(
+            currentAccountPicture: const CircleAvatar(
+              radius: 30.0,
+              backgroundImage: AssetImage('assets/images/logo2.png'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue[900],
+            ),
+            accountName: const Text('EJC Santa Teresinha'),
+            accountEmail: const Text('Bandinha')),
+
+        ListTile(
+            style: ListTileStyle.drawer,
+            leading: const Icon(Icons.home),
+            title: const Text('Inicio'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/home');
+            }),
+        //ListTile(
+        //    leading: const Icon(Icons.music_note),
+        //    title: const Text('Adicionar musica'),
+        //    onTap: () {
+        //      Navigator.of(context).pushReplacementNamed('/add');
+        //    }),
+        ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Sair'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            })
+      ],
+    ),
   );
 }
